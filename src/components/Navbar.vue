0d93a5bd34caf9647021346a4f8b6a2744415642
NnamukaChukwuka <govanokaka22@gmail.com>
2024-06-01 16:37:22 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup>
+   import gitHubIcon from './icons/gitHubIcon.vue';
+</script>
+
+<template>
+    <nav>
+        <div class="navbar">
+            <gitHubIcon class="github-icon" />
+            <h1>Nnamuka01's Github</h1>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+.navbar {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: #fff;
+  justify-content: center;
+  background-color: #96D6D6;
+  text-align: center;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.github-icon {
+  font-size: 3rem;
+}
+
+</style>
